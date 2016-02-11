a521c6dd433e484753ac3b13c9fbc8a50be2bd52
perillamint <perillamint@gentoo.moe>
2016-02-11 22:33:09 +0900

    Add grant

--- RightToWrite.js
+++ RightToWrite.js
@@ -6,5 +6,6 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @downloadURL https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.js
 // @updateURL   https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.meta.js
+// @grant       none
 // @license     GPLv3 or later
 // ==/UserScript==
