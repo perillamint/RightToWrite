c3d5d36a1531122e6913505dd43cfa98de3fcf2e
perillamint <perillamint@gentoo.moe>
2016-02-11 22:31:49 +0900

    Add script header

---
+++ RightToWrite.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        RightToWrite
+// @namespace   http://gmscript.gentoo.moe
+// @include     *
+// @version     0.1
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @downloadURL https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.js
+// @updateURL   https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.meta.js
+// @license     GPLv3 or later
+// ==/UserScript==
