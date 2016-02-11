e16ebf9327ec742f45c55ebc1a611df97c798e5a
perillamint <perillamint@gentoo.moe>
2016-02-12 00:51:37 +0900

    Rename file

--- RightToWrite.meta.js
+++ RightToWrite.meta.js
@@ -4,7 +4,7 @@
 // @include     *
 // @version     0.1
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
-// @downloadURL https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.js
+// @downloadURL https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.user.js
 // @updateURL   https://raw.githubusercontent.com/perillamint/RightToWrite/master/RightToWrite.meta.js
 // @grant       none
 // @license     GPLv3 or later
